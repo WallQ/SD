063ee2363b76a908af0598302c2bd9b0d5e0f295
Sérgio Félix <sergio98@outlook.pt>
2023-12-19 16:39:56 +0000

    IDK

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import Entities.User;
+import Enums.Role;
+import utils.JSONHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        List<User> userList = new ArrayList<>();
+        User user1 = new User(UUID.randomUUID(), "Sergio Felix", "8200615@estg.ipp.pt", "12345", Role.Captain);
+        User user2 = new User(UUID.randomUUID(), "Carlos Leite", "8200377@estg.ipp.pt", "54321", Role.Captain);
+
+        userList.add(user1);
+        userList.add(user2);
+
+        JSONHandler.saveUserToJson(userList, "data/users.json");
+
+        List<User> users = JSONHandler.loadUsersFromJson("data/users.json");
+        for (User user : users) {
+            System.out.println(user);
+        }
     }
 }
--- src/utils/JSONHandler.java
+++ src/utils/JSONHandler.java
@@ -1,21 +1,57 @@
 package utils;
 
+import Entities.User;
+import Enums.Role;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class JSONHandler {
-    public static JSONArray readJSONArrayFromFile(String fileName) throws IOException, ParseException {
-        return (JSONArray) new JSONParser().parse(new FileReader("data/" + fileName + ".json"));
+    public static void saveUserToJson(User user, String fileName) {
+        List<User> existingUsers = loadUsersFromJson(fileName);
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            existingUsers.add(user);
+            JSONArray jsonArray = new JSONArray();
+            for (User existingUser : existingUsers) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("id", user.getId().toString());
+                jsonObject.put("name", user.getName());
+                jsonObject.put("email", user.getEmail());
+                jsonObject.put("password", user.getPassword());
+                jsonObject.put("role", user.getRole().toString());
+                jsonArray.add(jsonObject);
+            }
+            fileWriter.write(jsonArray.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void writeToJSONFile(String jsonString, String fileName) throws IOException {
-        FileWriter file = new FileWriter("data/" + fileName + ".json");
-        file.write(jsonString);
-        file.close();
+    public static List<User> loadUsersFromJson(String fileName) {
+        List<User> userList = new ArrayList<>();
+        try (FileReader fileReader = new FileReader(fileName)) {
+            JSONParser jsonParser = new JSONParser();
+            JSONArray jsonArray = (JSONArray) jsonParser.parse(fileReader);
+            for (Object obj : jsonArray) {
+                JSONObject jsonObject = (JSONObject) obj;
+                UUID id = UUID.fromString((String) jsonObject.get("id"));
+                String name = (String) jsonObject.get("name");
+                String email = (String) jsonObject.get("email");
+                String password = (String) jsonObject.get("password");
+                Role role = Role.valueOf((String) jsonObject.get("role"));
+                userList.add(new User(id, name, email, password, role));
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 }
